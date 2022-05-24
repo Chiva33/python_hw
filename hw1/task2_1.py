a4f2a44a5ea2b08bacc7f2346461f573d718e6f5
Kirill K <francesc12008@gmail.com>
2022-05-24 20:57:54 +0300

    task2_1

---
+++ hw1/task2_1.py
@@ -0,0 +1,18 @@
+""" Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. """
+
+table = [
+    [False, False, False],
+    [False, False, True],
+    [False, True, False],
+    [True, False, False],
+    [False, True, True],
+    [True, True, False],
+    [True, False, True],
+    [True, True, True],
+]
+
+result = True
+for i in table:
+    if not (not (i[0] or i[1] or i[2])) == (not i[0] and not i[1] and not i[2]):
+        result = False
+print(result)
