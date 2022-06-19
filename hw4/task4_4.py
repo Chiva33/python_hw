b0c04ba0cfa9ac0167ad90cd9062c7d5dcb32b23
Kirill K <francesc12008@gmail.com>
2022-06-20 01:27:51 +0300

    task4_4

--- hw4/task4_4.py
+++ hw4/task4_4.py
@@ -4,15 +4,25 @@
 # пример записи в файл при n=3 ==> 33x^3 + 8x^2 + 64x + 85 = 0 при n=2 ==> 27x^2 + 95x + 79 = 0
 
 
-
-
+from dataclasses import replace
+from functools import reduce
 from random import randint, random
 
 
 n = int(input('Введите n:'))
 
-i = 0
 
-list = [str(randint(0, 100)) + 'x^' + str(n) + ' + ' for n in range(n, -1, -1)]
 
-print(list)
+def get_res(n):
+    list = [str(randint(0, 100)) + 'x^' + str(n) + ' + ' for n in range(n, -1, -1)]
+    list_str = reduce(lambda x, y: x + y, list)
+    list_str = list_str.replace("x^0", "")
+    list_str = list_str.replace("^1", "")
+    list_str = list_str.replace("1x", "x")
+    return list_str[: -3] + ' = 0'
+
+print(get_res(n))
+result = get_res(n)
+
+with open('get_res2.txt', 'w') as data:
+    data.write(result)
