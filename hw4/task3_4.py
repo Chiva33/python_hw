03c61bd9ef74cbd8e167736d8bce67faa1d0b181
Kirill Korsunsky <francesc12008@gmail.com>
2022-06-19 22:05:39 +0300

    task3_4

--- hw4/task3_4.py
+++ hw4/task3_4.py
@@ -3,24 +3,11 @@
 
 from random import randint
 
-
 list1 = []
 for i in range(0, 10):
     list1.append(randint(0, 10))
 print(list1)
 
 list2 = []
-print(len(list1))
-
-e = 0
-print(list1.count(e))
-for f in list1:
-    if list1.count(e) >= 2:
-        list1.pop(e)
-    else:
-        list2.append(list1[e])
-        
-
-
-
+[list2.append(i) for i in list1 if i not in list2]
 print(list2)
