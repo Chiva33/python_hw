f456de1507c488c408a958dc30c46a3e29f08933
Kirill K <francesc12008@gmail.com>
2022-06-23 00:17:44 +0300

    task1_5

--- hw5/task1_5.py
+++ hw5/task1_5.py
@@ -1,4 +1,10 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
 
-text = input(str('Введите текст:'))
+text = 'Результат эксперименабвта: работает лабви натуралабвьное средсабвтво от комаров из траабвв, ванили и эфирного масла?'
 
+def del_words(text):
+    text = list(filter(lambda x: 'абв' not in x, text.split()))
+    return " ".join(text)
+
+text = del_words(text)
+print(text)
